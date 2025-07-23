86bfd91e88c75d7cda18725c75346b45f66d6361
Anton <info@telecomus.ru>
2025-07-24 04:18:53 +0700

    add config server

--- telemetry/serialization/avro-schemas/src/main/java/ru/practicum/telemetry/deserialization/SensorEventDeserializer.java
+++
@@ -1,9 +0,0 @@
-package ru.practicum.telemetry.deserialization;
-
-import ru.yandex.practicum.kafka.telemetry.event.SensorEventAvro;
-
-public class SensorEventDeserializer extends BaseAvroDeserializer<SensorEventAvro> {
-    public SensorEventDeserializer() {
-        super(SensorEventAvro.getClassSchema());
-    }
-}
