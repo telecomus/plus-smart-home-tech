8c445d2c8fb6af60ceaa65689b79d601621071f4
Anton <info@telecomus.ru>
2025-07-24 04:46:16 +0700

    add config server

--- telemetry/analyzer/src/main/java/ru/yandex/practicum/telemetry/analyzer/model/ConditionOperation.java
+++
@@ -1,7 +0,0 @@
-package ru.yandex.practicum.telemetry.analyzer.model;
-
-public enum ConditionOperation {
-    EQUALS,
-    GREATER_THAN,
-    LOWER_THAN
-}
--- telemetry/serialization/avro-schemas/src/main/java/ru/practicum/telemetry/deserialization/SensorsSnapshotDeserializer.java
+++
@@ -1,9 +0,0 @@
-package ru.practicum.telemetry.deserialization;
-
-import ru.yandex.practicum.kafka.telemetry.event.SensorsSnapshotAvro;
-
-public class SensorsSnapshotDeserializer extends BaseAvroDeserializer<SensorsSnapshotAvro> {
-    public SensorsSnapshotDeserializer() {
-        super(SensorsSnapshotAvro.getClassSchema());
-    }
-}
