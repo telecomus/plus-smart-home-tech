2eef43da5d55d242a67d2c8f534f8722b85802dc
Anton <info@telecomus.ru>
2025-07-24 09:24:55 +0700

    add microservices

--- commerce/shopping-store/src/main/java/ru/practicum/yandex/mapper/ProductMapper.java
+++ commerce/shopping-store/src/main/java/ru/practicum/yandex/mapper/ProductMapper.java
@@ -9,7 +9,24 @@ import java.util.List;
 
 @Mapper(componentModel = MappingConstants.ComponentModel.SPRING)
 public interface ProductMapper {
-    Product productDtoToProduct(ProductDto productDto);
+    default Product productDtoToProduct(ProductDto productDto) {
+        if (productDto == null) {
+            return null;
+        }
+
+        Product product = new Product();
+        product.setProductId(productDto.getProductId());
+        product.setProductName(productDto.getProductName());
+        product.setDescription(productDto.getDescription());
+        product.setImageSrc(productDto.getImageSrc());
+        product.setQuantityState(productDto.getQuantityState());
+        product.setProductState(productDto.getProductState());
+        product.setRating(0); // Устанавливаем какое-то значение по умолчанию для рейтинга
+        product.setProductCategory(productDto.getProductCategory());
+        product.setPrice(productDto.getPrice());
+
+        return product;
+    }
 
     ProductDto productToProductDto(Product product);
 
